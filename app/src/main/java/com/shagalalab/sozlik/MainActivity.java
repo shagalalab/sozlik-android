a35404095161b4fc59da870188f5409a43b41f43
Atabek <atabekmur@gmail.com>
2018-03-01 23:14:07 +1100

    Add static analysis tools, fix problems found by these tools

--- app/src/main/java/com/shagalalab/sozlik/MainActivity.java
+++ app/src/main/java/com/shagalalab/sozlik/MainActivity.java
@@ -78,21 +78,11 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
+//        int id = item.getItemId();
+//
+//        if (id == R.id.nav_gallery) {
+//             Handle the camera action
+//        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
