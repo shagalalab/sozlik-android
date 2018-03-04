1b1a6bd0afbb0537d2aae266aa50ad84e9102c7d
QAREKEN <kalabaevshj@gmail.com>
2018-03-04 15:02:39 +0500

    abcd

--- app/src/main/java/com/shagalalab/sozlik/model/SozlikDatabase.java
+++ app/src/main/java/com/shagalalab/sozlik/model/SozlikDatabase.java
@@ -1,9 +1,6 @@
 package com.shagalalab.sozlik.model;
 
-import android.arch.persistence.db.SupportSQLiteOpenHelper;
 import android.arch.persistence.room.Database;
-import android.arch.persistence.room.DatabaseConfiguration;
-import android.arch.persistence.room.InvalidationTracker;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
