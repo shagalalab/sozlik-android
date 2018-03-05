0a0cff6f80699f001d1bdb7db459b379f0963fbf
QAREKEN <kalabaevshj@gmail.com>
2018-03-06 04:21:36 +0500

    BuildConfig.java, Manifest.java ham R.java fayllari .gitignore ga qosildi

--- app/src/main/java/com/shagalalab/sozlik/MainActivity.java
+++ app/src/main/java/com/shagalalab/sozlik/MainActivity.java
@@ -12,7 +12,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
     implements NavigationView.OnNavigationItemSelectedListener {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
