e70dbe004766d1a24736333e95552874327a31ce
QAREKEN <kalabaevshj@gmail.com>
2018-03-06 02:03:26 +0500

    drawer-duzetildi-ham-fab-oshirildi

--- app/src/main/java/com/shagalalab/sozlik/MainActivity.java
+++ app/src/main/java/com/shagalalab/sozlik/MainActivity.java
@@ -1,9 +1,6 @@
 package com.shagalalab.sozlik;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
